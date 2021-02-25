4e599be1733be283b5c86aee8572284a67afbfa2
Johannes Thumshirn <jth@kernel.org>
2021-02-25 15:38:37 +0100

    app: add rendering of markdown
    Add rendering of markdown files stored in the pages directory using
    templates in the templates directory.
    
    Signed-off-by: Johannes Thumshirn <jth@kernel.org>

--- app.py
+++ app.py
@@ -5,10 +5,21 @@ from flask_flatpages import FlatPages
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['FLATPAGES_EXTENSION'] = '.md'
 
-@app.route("/")
-def hello_world():
-    return 'Hello, World!'
+pages = FlatPages(app)
+
+@app.route('/<path:path>.html')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+@app.route('/')
+def index():
+    posts = [p for p in pages if "date" in p.meta]
+    sorted_pages = sorted(posts, reverse=True, key=lambda page:
+            datetime.strptime(page.meta["date"], "%d %b %y"))
+    return render_template('bloghome.html', pages=sorted_pages)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
---
+++ pages/blogpost.md
@@ -0,0 +1,6 @@
+title: Post title goes here
+date: 25 Feb 21
+author: Me
+description: Hello World
+
+Hello World
