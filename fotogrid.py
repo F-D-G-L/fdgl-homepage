3ca19319d763210a7e4f706f0cb4d1d1f64aa458
Markus Steinlein <47111718+Ayreonaut@users.noreply.github.com>
2021-03-09 16:59:44 +0100

    Enhanced fotogrid layout algorithm and made it more readable. Added unit tests.

--- fotogrid.py
+++ fotogrid.py
@@ -1,22 +1,32 @@
+# SPDX-License-Identifier: GPL-3.0
+
+
 def fotogrid(image_urls):
     """
     Provide a simple responsive fotogrid using flex layout.
     Depends on the css/fotogrid.css defined styles.
+    Use jinja {{ fotogrid([....]) }} from templates (registered in app.py)
     """
-    result = '<div class="fotorow">'
-    cols = 4
-    images = divmod(len(image_urls), cols)
-    images_per_col = images[0]
-    if images[1] > 0:
-        images_per_col += 1
-    nbr = 0
-    for img in image_urls:
-        if (nbr % images_per_col) == 0:
-            if nbr > 0:
-                result += '</div>'  # closing fotocolumn
-            result += '<div class="fotocolumn">'
-        result += '<img src="' + img + '" style="width:100%">'
-        nbr += 1
+    if len(image_urls) == 0:
+        return ""
 
-    result += "</div></div>"  # closing fotocolum, fotorow
-    return result
+    # prepare our 'bins', the columsn of the fotogrid
+    nbr_of_columns = 4
+    prepared_column = []
+    for i in range(nbr_of_columns):
+        prepared_column.append([])
+    # sort given image URL into bins
+    for i in range(len(image_urls)):
+        prepared_column[i % nbr_of_columns].append(image_urls[i])
+
+    # form HTML div structure from prepared bins
+    html = '<div class="fotorow">'
+    for column in prepared_column:
+        if len(column) > 0:
+            html += '<div class="fotocolumn">'
+            for url in column:
+                html += '<img src="' + url + '" style="width:100%">'
+            html += "</div>"  # closing fotocolumn
+    html += "</div>"  # closing fotorow
+
+    return html
